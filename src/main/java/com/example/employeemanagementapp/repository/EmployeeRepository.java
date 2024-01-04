f72b7f44b44e24b9a522694e7de259f28137f175
Traibi khadija <ktraybi@gmail.com>
2024-01-04 02:35:01 +0100

    Added pagination feature to improve data presentation

--- src/main/java/com/example/employeemanagementapp/repository/EmployeeRepository.java
+++ src/main/java/com/example/employeemanagementapp/repository/EmployeeRepository.java
@@ -1,16 +1,16 @@
 package com.example.employeemanagementapp.repository;
 
 import com.example.employeemanagementapp.entities.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-   List<Employee> findByNameContains(String keyword);
+   Page<Employee> findByNameContains(String keyword, Pageable pageable);
 
    //utilisons JPQL
    //@Query("select e from Employee e where e.name like %:x%")
--- src/main/java/com/example/employeemanagementapp/web/EmployeeController.java
+++ src/main/java/com/example/employeemanagementapp/web/EmployeeController.java
@@ -5,6 +5,8 @@ import com.example.employeemanagementapp.entities.Employee;
 import com.example.employeemanagementapp.repository.EmployeeRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,9 +28,14 @@ public class EmployeeController {
     }
 
     @GetMapping("/user/index")
-    public String index(Model model,@RequestParam(name="keyword",defaultValue = "") String kw){
-        List<Employee> employeeList = employeeRepository.findByNameContains(kw);
-        model.addAttribute("employeeList",employeeList);
+    public String index(Model model,
+                        @RequestParam(name = "page",defaultValue = "0") int page,
+                        @RequestParam(name = "size",defaultValue = "5") int size,
+                        @RequestParam(name="keyword",defaultValue = "") String kw){
+        Page<Employee> pageEmployees = employeeRepository.findByNameContains(kw, PageRequest.of(page,size));
+        model.addAttribute("employeeList",pageEmployees.getContent());
+        model.addAttribute("pages", new int[pageEmployees.getTotalPages()]);
+        model.addAttribute("currentPage",page);
         model.addAttribute("keyword",kw);
         return "employees";
     }
@@ -42,9 +49,9 @@ public class EmployeeController {
 
     @GetMapping("/admin/delete")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public String delete(Long id){
+    public String delete(Long id, String keyword,int page){
         employeeRepository.deleteById(id);
-        return "redirect:/user/index";
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
     }
 
     @GetMapping("/admin/editEmployee")
